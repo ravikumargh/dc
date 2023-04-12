3a0c446e2e6c8016263f7446ec0be7695e918a31
Ravikumar <ravikumargh13@gmail.com>
2023-04-12 12:06:11 +0530

    news cards

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,10 +2,38 @@ import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
 	selector: 'app-dashboard',
-	templateUrl: './dashboard.component.html'
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements AfterViewInit {
-
+	messages = [{
+		title: 'Financial Results/Dividend',
+		subject: 'To consider and approve the financial results for the period ended March 31, 2023 and dividend, if any',
+		date: '12-Apr-2023'
+	},
+	{
+		title: 'Dividend/Other business matters',
+		subject: 'To consider dividend and other business matters',
+		date: '13-Apr-2023'
+	},
+	{
+		title: 'Other business matters',
+		subject: 'To consider and approve the financial results for the period ended March 31, 2023 and dividend, if any',
+		date: '12-Apr-2023'
+	},
+	{
+		title: 'Financial Results/Dividend',
+		subject: 'To consider and approve the financial results for the period ended March 31, 2023 and dividend, if any',
+		date: '12-Apr-2023'
+	},
+	{
+		title: 'Financial Results/Dividend',
+		subject: 'To consider and approve the financial results for the period ended March 31, 2023 and dividend, if any',
+		date: '12-Apr-2023'
+	}];
+		
+    messagesLatest: Object[] = this.messages.slice(0, 3);
+	
 	ngAfterViewInit() { }
 
 }
